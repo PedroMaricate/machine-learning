2ec1494d4581817ae7e2a60d1708c9b6760e2237
PedroMaricate <pamaricates@gmail.com>
2025-08-20 19:02:47 -0300

    decision tree completa

--- docs/arvore-decisao/decision-tree.py
+++ docs/arvore-decisao/decision-tree.py
@@ -1,21 +1,63 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from io import StringIO
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
 def preprocess(df):
+    
     # Fill missing values
-    df['admission'].fillna(df['admission'].mode()[0], inplace=True)
+    df["admission"] = df["admission"].fillna("Deny") 
+    df['race'] = df['race'].fillna('Unknown')
+    
+    # Convert categorical variables
+    label_encoder = LabelEncoder()
+    df['gender'] = label_encoder.fit_transform(df['gender'])
+    df['international'] = label_encoder.fit_transform(df['international'])
+    df['major'] = label_encoder.fit_transform(df['major'])
+    df['race'] = label_encoder.fit_transform(df['race'])
+    df['work_industry'] = label_encoder.fit_transform(df['work_industry'])
+    df['admission'] = label_encoder.fit_transform(df['admission'])
+    
     # Select features
     features = ['gender', 'international', 'gpa', 'major', 'race', 'gmat', 'work_exp', 'work_industry', 'admission']
     return df[features]
-
+     
 # Load the dataset
 df = pd.read_csv('C:/Users/pedro.maricate/Downloads/baseMBA/MBA.csv')
 
-df = df.sample(n=10)
 # Preprocessing
 df = preprocess(df)
 
-# Display the first few rows of the dataset
-print(df)
+# Carregar o conjunto de dados
+x = df.drop(columns=['admission'])
+y = df['admission']
+
+# Dividir os dados em conjuntos de treinamento e teste
+x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,random_state=42)
+
+# Criar e treinar o modelo de árvore de decisão
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(x_train, y_train)
+
+# Avaliar o modelo
+accuracy = classifier.score(x_test, y_test)
+print(f"Accuracy: {accuracy:.2f}")
+tree.plot_tree(classifier)
+
+# Mostrar as regras da árvore no console
+from sklearn.tree import export_text
+rules = export_text(classifier, feature_names=list(x.columns))
+print(rules)
+
+# Para imprimir na página HTML
+buffer = StringIO()
+plt.savefig(buffer, format="svg")
+print(buffer.getvalue())
+
+
 
 
--- docs/arvore-decisao/main.md
+++ docs/arvore-decisao/main.md
@@ -1,17 +1,13 @@
 ## Exploração dos Dados
 
-Para o projeto foi utilizado o dataset [](http){:target='blank'}
+Para o projeto foi utilizado o dataset https://www.kaggle.com/datasets/taweilo/mba-admission-dataset
 
-## Pré-processamento
+O conjunto de dados utilizado contém informações de 6.194 candidatos a programas de MBA, incluindo variáveis acadêmicas, demográficas e profissionais, bem como o resultado da admissão. Entre as principais colunas, destacam-se: gênero, nacionalidade, GPA, área de formação, etnia, pontuação no GMAT, experiência de trabalho, setor de atuação e resultado da admissão.
 
-´´´ python
+## Códigos
+
 --8<-- "./docs/arvore-decisao/decision-tree.py"
-´´´
 
-## Divisão dos Dados
+## Considerações Finais
 
-## Treinamento do Modelo
-
-## Avaliação do Modelo
-
-## Conclusão
+O modelo treinado a partir da base de admissões de MBA apresentou uma acurácia de 0,77 (77%). Esse resultado indica que o algoritmo foi capaz de classificar corretamente a maioria das observações, demonstrando um desempenho satisfatório para generalização, ainda que exista espaço para melhorias futuras.
